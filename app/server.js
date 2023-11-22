92b137bc3678ee4262709e77f97f85be4c25a9aa
Julien Hilaire <julien.hilaire@imelmi.net>
2023-11-22 08:42:33 +0100

    filtre par domaine

--- app/server.js
+++ app/server.js
@@ -49,6 +49,60 @@ app.get('/api/tags', cors (), async (req, res) => {
   }
 });
 
+app.get('/api/domains', cors (), async (req, res) => {
+  try {
+    // Make an HTTP GET request to the back-end
+    const response = await axios.get(baseUrl+ "/domains", config);
+
+    // Send the data as the response to the client
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: 'Error fetching data'});
+  }
+});
+
+app.get('/api/domain/:domainId/',  cors (), async (req, res) => {
+  try {
+    const domainId = req.params.domainId;
+    // Make an HTTP GET request to the back-end
+    const response = await axios.get(baseUrl+ "/domains/"+domainId, config);
+    // Send the data as the response to the client
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: 'Error fetching data'});
+  }
+});
+
+
+app.get('/api/domain/:domain_name/pins',  cors (), async (req, res) => {
+  try {
+    const domainName = req.params.domain_name;
+    // Make an HTTP GET request to the back-end
+    const response = await axios.get(baseUrl+ "/pins?filterByFormula=(domain=\""+domainName+"\")", config);
+    https://api.airtable.com/v0/app7zNJoX11DY99UA/Tags?filterByFormula=AND({domain}="Maths")
+    // Send the data as the response to the client
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: 'Error fetching data'});
+  }
+});
+
+app.get('/api/domain/:domain_name/tags',  cors (), async (req, res) => {
+  try {
+    const domainName = req.params.domain_name;
+    // Make an HTTP GET request to the back-end
+    const response = await axios.get(baseUrl+ "/tags?filterByFormula=(domain=\""+domainName+"\")", config);
+    https://api.airtable.com/v0/app7zNJoX11DY99UA/Tags?filterByFormula=AND({domain}="Maths")
+        // Send the data as the response to the client
+        res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: 'Error fetching data'});
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
